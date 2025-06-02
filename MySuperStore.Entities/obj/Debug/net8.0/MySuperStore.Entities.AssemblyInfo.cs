017c5f1663df62a5c1b44557097f013fff9f947e
Mansur <Mansur@DESKTOP-HMUC4VN>
2025-06-02 17:37:50 +0200

    Added git attributes file

--- MySuperStore.Entities/obj/Debug/net8.0/MySuperStore.Entities.AssemblyInfo.cs
+++ MySuperStore.Entities/obj/Debug/net8.0/MySuperStore.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySuperStore.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52145fc88316c71884d85abb9c750571e895e8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySuperStore.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySuperStore.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MySuperStore.Utils/obj/Debug/net8.0/MySuperStore.Utils.AssemblyInfo.cs
+++ MySuperStore.Utils/obj/Debug/net8.0/MySuperStore.Utils.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MySuperStore.Utils")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52145fc88316c71884d85abb9c750571e895e8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MySuperStore.Utils")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MySuperStore.Utils")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
